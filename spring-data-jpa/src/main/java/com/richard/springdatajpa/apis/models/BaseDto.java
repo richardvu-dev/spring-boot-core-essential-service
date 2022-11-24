53981cdea650e8fc1e7b8d4232c1fd89b8de3aab
Richard Vu <richardvu.work@gmail.com>
2022-11-25 02:42:55 +0700

    Update Json Response

--- spring-data-jpa/src/main/java/com/richard/springdatajpa/apis/models/BaseDto.java
+++ spring-data-jpa/src/main/java/com/richard/springdatajpa/apis/models/BaseDto.java
@@ -6,6 +6,8 @@ import java.util.UUID;
 
 import javax.persistence.MappedSuperclass;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
@@ -20,18 +22,23 @@ public class BaseDto implements Serializable {
   @Schema(name = "Identity", description = "Identity of object")
   private UUID id;
 
+  @JsonProperty("created_by")
   @Schema(name = "Created By", description = "Created By of object")
   private String createdBy = "system";
 
-  @Schema(name = "Created By", description = "Created By of object")
+  @JsonProperty("update_by")
+  @Schema(name = "Update By", description = "Update By of object")
   private String updateBy = "system";
 
-  @Schema(name = "Created By", description = "Created By of object")
+  @JsonProperty("created_at")
+  @Schema(name = "Created At", description = "Created At of object")
   private LocalDateTime createdAt = LocalDateTime.now();
 
+  @JsonProperty("last_modified_at")
   @Schema(name = "Last Modified At", description = "Last Modified At of object")
   private LocalDateTime lastModifiedAt = LocalDateTime.now();
 
+  @JsonProperty("is_delete")
   @Schema(name = "Is Delete", description = "Is Delete of object")
   private Boolean isDelete;
 
